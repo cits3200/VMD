5848700da26134dacbd932d5371c7a173454cbe2
JINANG LU <lujinang@gmail.com>
2011-08-17 16:49:23 +0800

    add combinations with 5 characters

--- myguess.c
+++ myguess.c
@@ -1,4 +1,5 @@
 #include<unistd.h> 
+#include<stdbool.h>
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
@@ -9,6 +10,7 @@ int main(void)
 	char	base[52];
 	clock_t	start, finish;
 	double	duration;
+	char 	combinations[3];
 
 	base[0] = 'a';
 	base[26] ='A';
@@ -32,6 +34,33 @@ int main(void)
 				for(int k=0;k<26;k++)
 				{
 					printf("%c%c%c\n",base[j],base[z],base[k]);	
+				}
+			}
+		}
+		
+		printf("\n");
+	}
+	finish = clock();
+	duration = (double)(finish-start)/CLOCKS_PER_SEC;
+	printf("output all combinations with 3 characters takes %lf sec\n",duration);
+	
+	/*output all combinations with 5 characters*/
+	start = clock();
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<52;j++)
+		{
+			for(int z=0;z<52;z++)
+			{
+				for(int k=0;k<52;k++)
+				{	
+					for(int a=0;a<52;a++)
+					{
+						for(int b=0;b<52;b++)
+						{
+								printf("%c%c%c%c%c\n",base[j],base[z],base[k],base[a],base[b]);	
+						}	
+					}
 				}
 			}
 		}
